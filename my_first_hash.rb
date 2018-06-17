fac21b12ecc51472bd65f571d2e06639087e7583
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-17 17:00:36 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,8 +7,11 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale_bone_corsets)" => 5,"porcelain_vases" => 2, "oil_paintings" => 3}
-return the_manifest
+  the_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
 end
 
 def retrieval
